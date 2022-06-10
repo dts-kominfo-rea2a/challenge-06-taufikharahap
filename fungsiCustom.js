b502346502607f205873ccbbc047cb937f731e6e
Taufik Hidayat Harahap <hrptaufik42@gmail.com>
2022-06-10 10:20:32 +0700

    Membuat fungsi baca file

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -7,23 +7,45 @@ let file3 = "./data3.json";
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
-  file1 = val;
+    file1 = val;
 };
 let modifyFile2 = (val) => {
-  file2 = val;
+    file2 = val;
 };
 let modifyFile3 = (val) => {
-  file3 = val;
+    file3 = val;
 };
 
+const { hasSubscribers } = require("diagnostics_channel");
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData = (fnCallback) => {
+    let hasil = [];
+    [file1, file2, file3].map((file, index) => {
+        fs.readFile(file, "utf8", (err, data) => {
+            if (err) {
+                fnCallback(err, null);
+            }
+
+            if (index === 0) {
+                hasil.push(JSON.parse(data).message.substring(5));
+            } else if (index === 1) {
+                hasil.push(JSON.parse(data)[0].message.substring(5));
+            } else {
+                hasil.push(JSON.parse(data)[0].data.message.substring(5));
+            }
+        });
+    });
+    setTimeout(() => {
+        fnCallback(null, hasil);
+    }, 1000);
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
+    modifyFile1,
+    modifyFile2,
+    modifyFile3,
+    bacaData,
 };
--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { bacaData } = require("./fungsiCustom");
 // ! JANGAN DIMODIFIKASI
 // main function
 (() => {
-  console.log("Mari kita membaca data yah !");
-  console.log("====");
-  bacaData((err, data) => {
-    if (err) return console.log(err);
-    console.log(data?.join());
-  });
-  console.log("Walaupun tulisan ini diletakkan di setelah bacaData");
-  console.log("Namun tulisan ini akan ditampilkan terlebih dahulu");
-  console.log("Karena bacaData bersifat asynchronous");
-  console.log("====");
+    console.log("Mari kita membaca data yah !");
+    console.log("====");
+    bacaData((err, data) => {
+        if (err) return console.log(err);
+        console.log(data?.join());
+    });
+    console.log("Walaupun tulisan ini diletakkan di setelah bacaData");
+    console.log("Namun tulisan ini akan ditampilkan terlebih dahulu");
+    console.log("Karena bacaData bersifat asynchronous");
+    console.log("====");
 })();
